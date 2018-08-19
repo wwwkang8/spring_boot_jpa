aeb62699b77c37edf37abae7f13bfa2fc980a6b8
JeongHo <wwwkang8@gmail.com>
2018-08-19 18:09:21 +0900

    첫 커밋

---
+++ src/main/java/net/slipp/MyslippApplication.java
@@ -0,0 +1,12 @@
+package net.slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyslippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyslippApplication.class, args);
+	}
+}
