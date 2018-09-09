1452e1376764b64c9915d17ee618fb339547a146
JeongHo <wwwkang8@gmail.com>
2018-09-09 14:14:10 +0900

    템플릿 리팩토링,Restful URL 설정

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,31 +1,30 @@
 package net.slipp.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.User;
 import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users") //대표 URL. 이것 이후에 다른 url을 붙일 수 있다. url 중복 제거
 public class UserController {
 	
 	@Autowired   //스프링부트에서 알아서 생성해준다
 	private UserRepository userRepository;
 	
-	@PostMapping("/create")
+	@PostMapping("")
 	public String create(User user){
 		System.out.println("user : "+user);
 		userRepository.save(user);
-		return "redirect:/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model){
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
